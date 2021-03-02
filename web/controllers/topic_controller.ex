6472630f70433b81af621ca7ddc0207302afd1ee
anasilva65 <annadasilva89@hotmail.com>
2021-03-02 19:48:54 -0300

    metodo new, recebendo dois paramentos

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,6 +1,10 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
-  def new() do
-
+  def new(conn, params) do
+    IO.puts "++++"
+    IO.inspect conn
+    IO.puts "++++"
+    IO.inspect params
+    IO.puts "++++"
   end
 end
