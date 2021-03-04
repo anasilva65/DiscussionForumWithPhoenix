32306e22838aee35524e0b101eb5b873a98bae10
anasilva65 <annadasilva89@hotmail.com>
2021-03-04 15:20:29 -0300

    metodo que cria o topico e retorna adicionando no banco de dados

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,9 +2,19 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
   alias Discuss.Topic
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Discuss.Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Discuss.Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+
+    end
   end
 end
