bdbab3a18b1e980da541c2421b76b709f4473f37
anasilva65 <annadasilva89@hotmail.com>
2021-03-05 11:29:11 -0300

    controler atualizado

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,13 +3,13 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
   def new(conn, _params) do
-    changeset = Discuss.Topic.changeset(%Topic{}, %{})
+    changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Discuss.Topic.changeset(%Topic{}, topic)
+    changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
       {:ok, post} ->
@@ -24,5 +24,12 @@ defmodule Discuss.TopicController do
   def index(conn, _params) do
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
   end
 end
