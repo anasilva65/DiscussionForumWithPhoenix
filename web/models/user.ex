e358fbb080db6a0c5cf5dfc22d37d252d3103fc1
anasilva65 <annadasilva89@hotmail.com>
2021-03-08 18:18:46 -0300

    esquema dos usuarios com os campos da tabela, retornando a hora atualizada em que foi criado

---
+++ web/models/user.ex
@@ -0,0 +1,9 @@
+defmodule Discuss.User do
+  use Discuss.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+    timestamps()
+  end
