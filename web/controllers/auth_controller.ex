be08f8252fbdee9dcf67333e891d6a9d94dfea93
anasilva65 <annadasilva89@hotmail.com>
2021-03-08 20:09:08 -0300

    metodo privado de inserir ou atualizar o usuário, com a capacidade de pesquisar no banco de dados e detectar se um usuário está ou não fazendo login no site novamente ou se está efetuando login pela primeira vez.

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -10,4 +10,13 @@ defmodule Discuss.AuthController do
 
   end
 
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
+
 end
