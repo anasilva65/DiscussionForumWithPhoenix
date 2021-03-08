3dced4cae3f060dbb7204ae80bc63fcdc67e6ef5
anasilva65 <annadasilva89@hotmail.com>
2021-03-08 19:25:23 -0300

    metodo de alteração de dados, e garantir quais sao as propriedades que precisam estar presentes na estrututura

--- web/models/user.ex
+++ web/models/user.ex
@@ -5,5 +5,14 @@ defmodule Discuss.User do
     field :email, :string
     field :provider, :string
     field :token, :string
+
     timestamps()
   end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :provider, :token])
+    |> validate_required([:email, :provider, :token])
+  end
+
+end
