8f93da0d6cda0870ac64e6a3e6871a87b4f9927a
Ana Silva <annadasilva89@hotmail.com>
2021-03-18 19:29:07 -0300

    implementacao do client - websockets

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -1,13 +1,15 @@
 import {Socket} from "phoenix"
-
 let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
-
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("comments:1", {})
+const createSocket = topicId => {
+let channel = socket.channel(`comments:${topicId}`, {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+}
+
+window.createSocket = createSocket
 
 export default socket
