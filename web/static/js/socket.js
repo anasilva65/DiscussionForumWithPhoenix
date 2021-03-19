79f74abade9a2de09e14bdf94612bf78dc5de7d2
Ana Silva <annadasilva89@hotmail.com>
2021-03-18 23:06:06 -0300

    funcao que renderiza a lista de comentarios

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -3,19 +3,27 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 // Now that you are connected, you can join channels with a topic:
-const createSocket = topicId => {
+const createSocket = (topicId) =>
+ {
 let channel = socket.channel(`comments:${topicId}`, {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
-  document.querySelector('button').addEventListener('click', () => {
+  document.querySelector('button').addEventListener('click', () => 
+  {
     const content = document.querySelector('textarea').value;
 
     channel.push('comment:add', { content: content });
   });
+};
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return commentTemplate(comment);
+  });
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
 }
 
-window.createSocket = createSocket
-
-export default socket
+window.createSocket = createSocket;
