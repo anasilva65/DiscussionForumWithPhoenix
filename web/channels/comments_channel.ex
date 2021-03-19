969e999a2df76972216e0bbe4d1ea011c78fbec1
Ana Silva <annadasilva89@hotmail.com>
2021-03-18 21:19:12 -0300

    metodo que adiciona os comentarios no banco de dados

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -10,6 +10,21 @@ defmodule Discuss.CommentsChannel do
   end
 
   def handle_in(name, %{"content" => content}, socket) do
-    {:replay, :ok, socket}
+    topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
+
+    changeset = topic
+      |> build_assoc(:comments, user_id: user_id)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new",
+          %{comment: comment}
+        )
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
